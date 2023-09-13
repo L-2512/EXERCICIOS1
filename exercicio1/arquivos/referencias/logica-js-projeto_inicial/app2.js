9fe3ffabb5ae0312b19dcdf57857fe9ccac30125
Leo Teixeira <144177474+L-2512@users.noreply.github.com>
2023-09-13 20:58:44 -0300

    Add files via upload

---
+++ exercicio1/arquivos/referencias/logica-js-projeto_inicial/app2.js
@@ -0,0 +1,8 @@
+function exercicio1() {
+alert("Ex 1")
+let numero1 = prompt("Escreva um número");
+let numero2 = prompt("Escreva outro número");
+let resultado = numero1 + numero2;
+alert(resultado);
+}
+
